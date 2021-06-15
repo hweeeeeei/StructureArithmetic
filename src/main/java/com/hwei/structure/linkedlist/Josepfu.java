cf443bd32909dceaa5e03f82c97f2568cbdbcc06
giaogiao <giao@gmail.com>
2021-06-15 21:15:28 +0800

    约瑟夫问题

--- src/main/java/com/hwei/structure/linkedlist/Josepfu.java
+++ src/main/java/com/hwei/structure/linkedlist/Josepfu.java
@@ -11,11 +11,14 @@ public class Josepfu {
 
         CircleSingleLinkList circleSingleLinkList = new CircleSingleLinkList();
 
-        circleSingleLinkList.addBoy(5);
+        // 初始化人数
+        circleSingleLinkList.initBoy(50000);
 
+        // 遍历
         circleSingleLinkList.show();
 
-
+        // 报数
+        circleSingleLinkList.popBoy(100, 4000, 50000);
     }
 }
 
@@ -25,8 +28,81 @@ class CircleSingleLinkList {
     //创建一个first节点,当前没有编号
     private Boy first = null;
 
-    // 添加小孩节点,构成环形的链表
-    public void addBoy(int nums) {
+    /**
+     * 根据用户输入 让小孩出圈
+     *
+     * @param startNo  从第几个开始数
+     * @param countNum 数几下
+     * @param nums     共有多少
+     */
+    public void popBoy(int startNo, int countNum, int nums) {
+
+        // 对数据进行校验
+        if (first == null) {
+            System.out.println("first == null");
+            return;
+        }
+        if (startNo <= 0) {
+            System.out.println("startNo <= 0");
+            return;
+        }
+        if (startNo > nums) {
+            System.out.println("startNo > nums");
+            return;
+        }
+
+        // 辅助指针   指向链表最后一个节点
+        Boy helper = first;
+
+        // 找到最后一个节点
+        while (true) {
+            if (helper.getNext() == first) {
+                break;
+            } else {
+                // 指针位移
+                helper = helper.getNext();
+            }
+        }
+
+        // 报数前, 移动到开始报数的位置,  first和helper 移动  k-1次
+        for (int i = 0; i < startNo - 1; i++) {
+            // 指针位移
+            first = first.getNext();
+            // 指针位移
+            helper = helper.getNext();
+        }
+
+        // 开始报数 first 和helper同时移动m-1次 , 循环出圈
+        //  一个循环操作, 直到圈中只有一个人
+        while (true) {
+            if (helper == first) {
+//                System.out.println("最后节点");
+                break;
+            } else {
+
+                //first 和helper同时移动m-1次
+                for (int i = 0; i < countNum - 1; i++) {
+                    // 指针位移
+                    first = first.getNext();
+                    // 指针位移
+                    helper = helper.getNext();
+                }
+
+                // 循环后 first 指向的节点 就是需要出圈的节点
+                System.out.println("出圈" + first.getNo());
+
+                // 出圈操作
+                first = first.getNext();
+                helper.setNext(first);
+
+            }
+        }
+        System.out.println("最后留在圈中" + first.getNo());
+
+    }
+
+    // 初始化添加小孩节点,构成环形的链表
+    public void initBoy(int nums) {
         // 对nums数据校验
         if (nums < 1) {
             System.out.println("nums不正确");
@@ -64,7 +140,8 @@ class CircleSingleLinkList {
         Boy curBoy = first;
 
         while (true) {
-            System.out.println(curBoy.getNo());
+
+            System.out.println("小孩的编号:" + curBoy.getNo());
 
             // 指针位移1
             curBoy = curBoy.getNext();
